cebd37f17a1b0d93f0cb7a074e34f06c202f988c
AKHRIF Jaafar <akhrif.jaafar.dev@gmail.com>
2021-02-26 10:53:56 +0100

    Code Souce et Présentation

--- Réalisation -Code Source/js/main.js
+++ Réalisation -Code Source/js/main.js
@@ -1,12 +1,14 @@
-function afficheName(){
-    Name = document.getElementById("name").value;
-    document.getElementById("affiche").innerHTML += Name;
-}
-function affichePhone(){
-    Phone = document.getElementById("phone").value;
-    document.getElementById("affiche").innerHTML += Phone;
-}
-function afficheEmail(){
-    Email = document.getElementById("email").value;
-    document.getElementById("affiche").innerHTML += Email;
-}
+function Information(){
+  
+           var Name = document.getElementById("name").value;
+            document.getElementById("affiche").innerHTML += "<b id=op>Nom:</b>&nbsp;" + Name + "<br>";
+
+           var Phone = document.getElementById("phone").value;
+            document.getElementById("affiche").innerHTML += "<b id=op>Téléphone:</b>&nbsp;" + Phone + "<br>";
+
+           var Email = document.getElementById("email").value;
+            document.getElementById("affiche").innerHTML += "<b id=op>Email:</b>&nbsp;" + Email + "<br><br>";
+          
+
+        }
+
