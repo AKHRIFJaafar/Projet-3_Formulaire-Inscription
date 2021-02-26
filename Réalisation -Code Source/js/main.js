f2d122da5ffed88e2fb1b08565bf63809b3c97b0
Jaafar97 <akhrifjaafar2@gmail.com>
2021-02-26 02:28:03 +0100

    Organisation Projet

---
+++ Réalisation -Code Source/js/main.js
@@ -0,0 +1,12 @@
+function afficheName(){
+    Name = document.getElementById("name").value;
+    document.getElementById("affiche").innerHTML += Name;
+}
+function affichePhone(){
+    Phone = document.getElementById("phone").value;
+    document.getElementById("affiche").innerHTML += Phone;
+}
+function afficheEmail(){
+    Email = document.getElementById("email").value;
+    document.getElementById("affiche").innerHTML += Email;
+}
