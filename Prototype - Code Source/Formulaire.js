b183941fe82354f7bfc2938f8645748049d9b6d8
AKHRIF Jaafar <akhrif.jaafar.dev@gmail.com>
2021-02-26 11:15:14 +0100

    Ajoute de Protype

---
+++ Prototype - Code Source/Formulaire.js
@@ -0,0 +1,30 @@
+function Nom(){
+    var nom = document.getElementById("nom").Value;
+    document.getElementById("result-container").innerHTML += nom ;
+}
+function Prénom(){
+    var prénom = document.getElementById("prénom").Value;
+    document.getElementById("result-container").innerHTML += prénom; 
+}
+function tél(){
+    var tél = document.getElementById("tél").Value;
+    document.getElementById("result-container").innerHTML += tél;
+}
+function Email(){
+    var email = document.getElementById("email").Value;
+    document.getElementById("result-container").innerHTML += email;
+}
+function Adresse(){
+    var adresse = document.getElementById("adresse").Value;
+    document.getElementById("result-container").innerHTML += adresse;
+}
+
+
+    
+
+    
+
+  
+
+
+
