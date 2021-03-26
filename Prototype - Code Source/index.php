3e10cda83149a42310020a743676a2613086ff90
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-03-26 10:54:38 +0100

    Prototype Avec Session

--- Prototype - Code Source/index.php
+++ Prototype - Code Source/index.php
@@ -1,36 +1,20 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <script type = "text/javascript" src = "custom.js" ></script>
-    <title>Inscription</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <title>Document</title>
 </head>
-
 <body>
-    <div class="container">
-        <form action="donner.php" method="POST">
-            <h2 id="prototype">PROTTYPE </h2>
-          
-               <label id="Label" >Name</label>
-               <input type="text" class="nom" name="nom" placeholder="Your Name" ><br><br>
-            
-                <label id="Label" >Phone</label>
-                <input type="text"  class="nom" name="phone" placeholder="Your Phone" 
-                ><br><br>
-    
-                <!-- <label id="Label" >Email</label>
-                <input type="text" class="nom" placeholder="Your Email" ><br><br><br> -->
-
-            <input type="submit" value="Ajouter" >
-        </form>
-
-
-    </div>
-
-
+    <h1>Formulaire d'inscription</h1>
+    <form action="affichage.php" target="_blank" method="get" >
+        <label for="" >Nom</label>
+        <input name="nom" type="text">
+        <button type="submit">Enregistrer</button>
+    </form>
+</body>
+</html>
     
     
 </body>
